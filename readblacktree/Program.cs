77034c18a4f2f19f970d41b3873e44ccdc5dcc3d
jean-mathieu vermosen <vermosen@yahoo.com>
2020-11-15 10:40:42 +0100

    up

---
+++ readblacktree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace readblacktree
+{
+    public class Tree 
+    { 
+        private class Node 
+        {
+            Node m_lhs;
+            Node m_rhs;
+            IComparable value;
+        };
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
